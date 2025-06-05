fad2ef97ac79269867d8c38e266c6f42d7aa3233
mojunsul <sdg2435@naver.com>
2025-06-05 11:20:11 +0900

    [chore] firebase 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
 
     // ktlint
     id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -57,6 +58,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
 
     // ktlint
     id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
+    alias(libs.plugins.google.gms.google.services) apply false
 }
