c6e352b84310b00833ded1db545a12ecaa7d171a
mojunsul <sdg2435@naver.com>
2025-06-07 22:40:08 +0900

    [feat] findById 추가

--- app/src/main/java/com/konkuk/kindmap/repository/StoreRepository.kt
+++ app/src/main/java/com/konkuk/kindmap/repository/StoreRepository.kt
@@ -1,5 +1,3 @@
-package com.konkuk.kindmap.repository
-
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -8,25 +6,47 @@ import com.konkuk.kindmap.model.StoreEntity
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
-import kotlin.jvm.java
+import kotlinx.coroutines.tasks.await
 
-class StoreRepository (private val table: DatabaseReference){
-    fun getAllItems(): Flow<List<StoreEntity>> = callbackFlow {
+class StoreRepository(private val table: DatabaseReference) {
+
+    fun findAll(): Flow<List<StoreEntity>> = callbackFlow {
+        try {
+            val snapshot = table.get().await()
+            val stores = snapshot.children.mapNotNull {
+                it.getValue(StoreEntity::class.java)
+            }
+            trySend(stores).isSuccess
+            close()
+        } catch (e: Exception) {
+            close(e)
+        }
+
+        awaitClose { }
+    }
+
+    fun findById(shId: Long): Flow<StoreEntity?> = callbackFlow {
+        val query = table.orderByChild("sh_id").equalTo(shId.toDouble())
+
         val listener = object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                val itemList = snapshot.children.mapNotNull {
-                    it.getValue(StoreEntity::class.java)
-                }
-                trySend(itemList)
+                val store = snapshot.children
+                    .mapNotNull { it.getValue(StoreEntity::class.java) }
+                    .firstOrNull()
+
+                trySend(store).isSuccess
+                close()
             }
 
             override fun onCancelled(error: DatabaseError) {
                 close(error.toException())
             }
         }
-        table.addValueEventListener(listener)
+
+        query.addListenerForSingleValueEvent(listener)
+
         awaitClose {
-            table.removeEventListener(listener)
+            query.removeEventListener(listener)
         }
     }
 }
