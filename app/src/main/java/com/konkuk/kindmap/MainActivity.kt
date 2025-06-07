ccb8319b0d5f04a0dbf163aaffb2338512db67de
mojunsul <sdg2435@naver.com>
2025-06-07 22:40:51 +0900

    [delete] StoreViewModel 삭제

--- app/src/main/java/com/konkuk/kindmap/MainActivity.kt
+++ app/src/main/java/com/konkuk/kindmap/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.konkuk.kindmap
 
+import StoreRepository
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -14,27 +14,40 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
+import com.google.firebase.database.FirebaseDatabase
 import com.konkuk.kindmap.ui.theme.MyApplicationTheme
-import com.konkuk.kindmap.viewmodel.StoreViewModel
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-
-    private val storeViewModel: StoreViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        //DB 연결 테스트
+        // ### DB 호출시 참고하세요 ###
+        // Firebase "STORE" 참조 가져오기
+        val databaseRef = FirebaseDatabase.getInstance().reference.child("STORE")
+        val storeRepository = StoreRepository(databaseRef)
+
+        // DB 테스트
         lifecycleScope.launch {
-            storeViewModel.storeList.collect { stores ->
-                Log.d("FirebaseTest", "Fetched ${stores.size} stores")
-                for (store in stores) {
-                    Log.d("FirebaseTest", "ID: ${store.sh_id}, Store: ${store.sh_name}, Address: ${store.sh_addr}, GeoHash: ${store.geohash}")
+            storeRepository.findAll().collectLatest { stores ->
+                Log.d("FirebaseTest findAll", "Fetched ${stores.size} stores")
+                stores.forEach { store ->
+                    Log.d("FirebaseTest findAll", "ID: ${store.sh_id}, Name: ${store.sh_name}, Addr: ${store.sh_addr}, GeoHash: ${store.geohash}")
+                }
+            }
+
+            storeRepository.findById(10426L).collectLatest { store ->
+                if (store != null) {
+                    Log.d("FirebaseTest findById", "ID: ${store.sh_id}, Name: ${store.sh_name}, Addr: ${store.sh_addr}, GeoHash: ${store.geohash}")
+                } else {
+                    Log.d("FirebaseTest findById", "해당 ID의 가게를 찾을 수 없습니다.")
                 }
             }
         }
+        // ### ###
 
         setContent {
             MyApplicationTheme {
